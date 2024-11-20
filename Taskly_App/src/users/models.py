2bbf823d825ee5e2a1063d3e45bd0d2f743224c7
Ayim William <odhisayim@gmail.com>
2024-11-20 17:21:23 +0300

    changed adminprofile

--- Taskly_App/src/users/models.py
+++ Taskly_App/src/users/models.py
@@ -19,4 +19,7 @@ user_profile_name_path = GenerateProfileImagePath()
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.FileField(upload_to=user_profile_name_path)
+    image = models.FileField(upload_to=user_profile_name_path, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}\'s Profile"

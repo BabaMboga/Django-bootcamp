0640764ddee196883dc368ccbd5566a1448019d9
Ayim William <odhisayim@gmail.com>
2024-11-20 16:43:13 +0300

    added users app

--- Taskly_App/src/taskful_api/settings.py
+++ Taskly_App/src/taskful_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@&t-=x4dr(l)*3ui#=e=-*57a4utxrzgqw(sm$n%jm0()i=f)k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [

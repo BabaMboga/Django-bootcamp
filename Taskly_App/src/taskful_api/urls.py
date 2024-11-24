61b5c1052ec8f1aaec7f0143f05259d45b4c2643
Ayim William <odhisayim@gmail.com>
2024-11-24 11:50:57 +0300

    finished setting up the routes for the router

--- Taskly_App/src/taskful_api/urls.py
+++ Taskly_App/src/taskful_api/urls.py
@@ -20,10 +20,10 @@ from django.urls import path, include
 from users import router as users_api_router
 
 api_url_patterns = [
-    path(r'/accounts', include(users_api_router.router.urls))
+    path(r'accounts/', include(users_api_router.router.urls))
 ]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/api', include(api_url_patterns))
+    path('api/', include(api_url_patterns))
 ]
